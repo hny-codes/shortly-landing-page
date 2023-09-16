c49ff7c8f75adab7f62e8ad633814f11ba9c51b7
Henry <hnguyhenry@protonmail.com>
2023-09-15 17:28:44 -0700

    Add animation for menu opening and closing

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
       fontFamily: {
         Poppins: ['Poppins', 'Poppins-Bold', ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        menuOpen: {
+          from: { opacity: 0, transform: 'translateY(50px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        menuClose: {
+          from: { opacity: 1, transform: 'translateY(0)' },
+          to: { opacity: 0, transform: 'translateY(50px)' },
+        },
+      },
+      animation: {
+        'menu-open': 'menuOpen 0.3s ease',
+        'menu-close': 'menuClose 0.3s ease',
+      },
     },
   },
   plugins: [],
