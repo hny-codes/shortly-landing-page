0da11081d36edd0a2d6c1afe76460842bb7188f7
Henry <hnguyhenry@protonmail.com>
2023-09-15 18:07:29 -0700

    Add desktop view for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,18 +6,18 @@ export default function Navbar() {
   const [menu, setMenu] = useState(false);
 
   const handleClick = () => {
-    setMenu((prevState) => !prevState)
-  }
+    setMenu((prevState) => !prevState);
+  };
 
   return (
     <nav data-test='nav'>
-      <div className='relative flex justify-between items-center w-[90%] mx-auto py-6'>
+      <div className='relative flex justify-between items-center w-[90%] mx-auto py-6 sm:max-w-[--max-w]'>
         {/* Menu Popup */}
         <div
           data-test='menu'
           className={`${
             menu ? 'visible animate-menu-open' : ' invisible animate-menu-close'
-          }  rounded-xl absolute top-20 left-0 w-full py-10 px-8 text-lg font-bold flex flex-col items-center gap-4 bg-[--clr-primary-violet] text-white transition-all`}
+          }  rounded-xl absolute top-20 left-0 w-full py-10 px-8 text-lg font-bold flex flex-col items-center gap-4 bg-[--clr-primary-violet] text-white transition-all md:hidden`}
         >
           <a href='/' className='w-full text-center py-2'>
             Features
@@ -37,17 +37,62 @@ export default function Navbar() {
           </a>
         </div>
 
-        {/* Mobile */}
-        <img
-          src={Logo.src}
-          alt='Logo image'
-          width={Logo.width}
-          height={Logo.height}
-        />
+        <div className='md:flex md:gap-8 md:items-center [&>a]:text-[--clr-neutral-gray]'>
+          <img
+            src={Logo.src}
+            alt='Logo image'
+            width={Logo.width}
+            height={Logo.height}
+          />
+          <a
+            data-test='desktop-link'
+            href='/'
+            className='w-full text-center py-2 hidden md:block hover:text-black transition'
+          >
+            Features
+          </a>
+          <a
+            data-test='desktop-link'
+            href='/'
+            className='w-full text-center py-2 hidden md:block hover:text-black transition'
+          >
+            Pricing
+          </a>
+          <a
+            data-test='desktop-link'
+            href='/'
+            className='w-full text-center py-2 hidden md:block hover:text-black transition'
+          >
+            Resources
+          </a>
+        </div>
 
-        <button onClick={handleClick} data-test='menu-btn'>
+        {/* Mobile */}
+        <button
+          onClick={handleClick}
+          data-test='menu-btn'
+          className='md:hidden'
+        >
           <img src={Menu.src} alt='' width={Menu.width} height={Menu.height} />
         </button>
+
+        {/* Desktop */}
+        <div className='md:flex md:gap-8 md:items-center hidden'>
+          <a
+            data-test='desktop-link'
+            href='/'
+            className='w-full text-center py-2 text-[--clr-neutral-gray] hover:text-black transition'
+          >
+            Login
+          </a>
+          <a
+            data-test='desktop-link'
+            href='/'
+            className='btn text-white md:whitespace-nowrap py-2 px-6'
+          >
+            Sign Up
+          </a>
+        </div>
       </div>
     </nav>
   );
