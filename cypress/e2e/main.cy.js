d33b51f799bd327a44949d9e1f938db61f8246a0
Henry <hnguyhenry@protonmail.com>
2023-09-15 17:45:50 -0700

    Add test for navbar mobile

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,5 +1,29 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Main Test', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.viewport('iphone-8');
+  });
+  it('Test for functional nav bar', () => {
+    // Should exist
+    cy.get('[data-test="nav"]').should('exist');
+
+    // Menu Clicking
+    cy.get('[data-test="menu-btn"]').as('menu-btn');
+    cy.get('[data-test="menu"]').as('menu');
+
+    // Menu should not initially exist
+    cy.get('@menu-btn').should('exist');
+    cy.get('@menu').should('be.hidden');
+
+    // After click, menu should exist
+    cy.get('@menu-btn').click();
+    cy.get('@menu').should('not.be.hidden');
+    cy.get('@menu').within(() => {
+      cy.get('a').should('have.length', 5);
+    });
+
+    // Click again to hide menu
+    cy.get('@menu-btn').click();
+    cy.get('@menu').should('be.hidden');
+  });
+});
