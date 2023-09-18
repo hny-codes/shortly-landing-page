c05e62321249b63770a46215f1fe11c8d80b2701
Henry <hnguyhenry@protonmail.com>
2023-09-18 16:55:44 -0700

    Add tests for linkbox and link components

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -34,10 +34,10 @@ describe('Main Test', { retries: 3 }, () => {
     cy.get('[data-test="desktop-link"]').should('have.length', 5);
   });
 
-  it.only('Hero Component', () => {
+  it('Hero Component', () => {
     const title = 'More than just shorter links';
     const subtitle =
-      'Build your brand\'s recognition and get detailed insights on how your links are performing.';
+      "Build your brand's recognition and get detailed insights on how your links are performing.";
 
     // Should exist
     cy.get('[data-test="hero"]').should('exist');
@@ -46,5 +46,37 @@ describe('Main Test', { retries: 3 }, () => {
     cy.get('[data-test="hero-subtitle"]').contains(subtitle);
 
     cy.get('[data-test="hero-btn"]').should('exist');
+  });
+
+  it.only('Link Box', () => {
+    const link = 'https://astro.build/';
+    cy.get('[data-test="link-input"]').as('input');
+    cy.get('[data-test="shorten-btn"]').as('btn');
+    cy.get('[data-test="link-list"]').as('list');
+
+    // No errors should be valid upon initial launch
+    cy.get('[data-test="error"]').should('not.exist');
+
+    // No shorten links upon initial launch
+    cy.get('[data-test="list-item"]').should('not.exist');
+
+    // Test for empty input
+    cy.get('@btn').click();
+    cy.get('[data-test="error"]').should('exist');
+
+    cy.reload();
+
+    // Test for invalid link
+    cy.get('@input').type('hps:/astro./');
+    cy.get('@btn').click();
+    cy.get('[data-test="error"]').should('exist');
+
+    // Test for valid link
+    cy.get('@input').clear().type(link);
+    cy.get('[data-test="error"]').should('not.exist');
+    cy.get('@btn').click();
+
+    cy.get('[data-test="list-item"]').should('exist');
+    
   });
 });
